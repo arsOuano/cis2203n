218b1680d05ed0d1dc2d3d032932929a9fa6ea74
aouano <23100490@usc.edu.ph>
2026-03-05 22:03:03 +0800

    part 1

---
+++ app/src/main/java/com/example/exercise1/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.exercise1;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+        setTitle("ID: 23100490 ");
+        
+        Button myButton = findViewById(R.id.btnChangeName);
+        final TextView myText = findViewById(R.id.tvDepartment);
+        
+        if (myButton != null) {
+            myButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (myText != null) {
+                        myText.setText("Name of the Department Chairwoman");
+                    }
+                }
+            });
+        }
+    }
+}
