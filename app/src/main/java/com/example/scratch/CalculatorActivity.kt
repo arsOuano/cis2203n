cdd5e36ef6a67f01db5fae33f5af6991179ec610
aouano <23100490@usc.edu.ph>
2026-04-21 00:36:59 +0800

    Variable var

---
+++ app/src/main/java/com/example/scratch/CalculatorActivity.kt
@@ -0,0 +1,15 @@
+package com.example.scratch
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CalculatorActivity : AppCompatActivity() {
+    var currentInput = ""
+    var firstOperand = 0.0
+    var currentOperator = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_calculator)
+    }
+}
