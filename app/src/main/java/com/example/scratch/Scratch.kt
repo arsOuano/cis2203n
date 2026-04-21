71adaf42cc1718fef330d230959b4a2d0e296ae0
aouano <23100490@usc.edu.ph>
2026-04-21 19:22:25 +0800

    Task B2

--- app/src/main/java/com/example/scratch/Scratch.kt
+++ app/src/main/java/com/example/scratch/Scratch.kt
@@ -5,4 +5,11 @@ fun main() {
     var age = 21
     age += 1
     println("My name is $name and I am $age years old.")
+
+    val greeting = greetUser(name)
+    println(greeting)
+}
+
+fun greetUser(name: String): String {
+    return "Hello, $name! Welcome to Kotlin."
 }
